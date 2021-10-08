8add9c59eba5b057409ba0db2058af81d3a71831
archepro84 <archepro84@gmail.com>
2021-10-09 00:05:22 +0900

    2021-10-09

--- Chapter1_Pythonic/Way3.py
+++ Chapter1_Pythonic/Way3.py
@@ -14,7 +14,7 @@ def fn_2():
     print(a.encode())
 
 
-# Bytes나 Str 인스턴스를 받아서 항상 Str을 반환
+# Bytes나 Str 인스턴스를 받아 항상 Str을 반환
 def fn_3():
     def to_str(bytes_or_str):
         if isinstance(bytes_or_str, bytes):
@@ -28,7 +28,7 @@ def fn_3():
     print(repr(to_str(b'\xed\x95\x9c')))  # UTF-8에서 한글은 3Byte
 
 
-# Bytes나 Str 인스턴스르 받아서 항상 Bytes를 반환한다.
+# Bytes나 Str 인스턴스를 받아서 항상 Bytes를 반환한다.
 def fn_4():
     def to_bytes(bytes_or_str):
         if isinstance(bytes_or_str, str):
@@ -104,23 +104,39 @@ def fn_14():
     pass
 
 
+# 파일 핸들과 관련된 연산들이 Default로 유니코드 문자열을 요구하고 이진 Byte 문자열을 요구하지 않는다.
 def fn_15():
+    with open('data.bin', 'w') as f:
+        f.write(b'\xf1\xf2\xf3\xf4\xf5')
     pass
 
 
+# With open을 wb로 변경해 이진 텍스트 쓰기모드로 정의한다.
 def fn_16():
+    with open('data.bin', 'wb') as f:
+        f.write(b'\xf1\xf2\xf3\xf4\xf5')
     pass
 
 
+# 이진 파일을 'r' 옵션으로 읽으려고 시도할 때 오류가 발생한다.
 def fn_17():
+    with open('data.bin', 'r') as f:
+        data = f.read()
     pass
 
 
+# 이진 읽기 모드 'rb' 옵션으로 열면 해결할 수 있다.
 def fn_18():
+    with open('data.bin', 'rb') as f:
+        data = f.read()
     pass
 
 
+# open 함수의 encoding 피라미터를 명시하면 플랫폼에 따라 동작이 달라져 에러가 발생하는 것을 막을 수 있다.
 def fn_19():
+    with open('data.bin', 'r', encoding='cp1252') as f:
+        data = f.read()
+    assert data == 'noooo'
     pass
 
 
@@ -138,10 +154,10 @@ if __name__ == '__main__':
     # fn_11()
     # fn_12()
     # fn_13()
-    fn_14()
+    # fn_14()
     # fn_15()
     # fn_16()
     # fn_17()
     # fn_18()
-    # fn_19()
+    fn_19()
     pass
---
+++ Chapter1_Pythonic/Way4.py
@@ -0,0 +1,27 @@
+def fn_1():
+    pass
+
+
+def fn_2():
+    pass
+
+
+def fn_3():
+    pass
+
+
+def fn_4():
+    pass
+
+
+def fn_5():
+    pass
+
+
+if __name__ == '__main__':
+    fn_1()
+    # fn_2()
+    # fn_3()
+    # fn_4()
+    # fn_5()
+    pass
