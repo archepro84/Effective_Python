804ac29b170588867e2ff8d63d2bd49ddeeac907
archepro84 <archepro84@gmail.com>
2021-10-10 17:53:23 +0900

    2021-10-10

--- Chapter1_Pythonic/Way4.py
+++ Chapter1_Pythonic/Way4.py
@@ -122,39 +122,137 @@ def fn_10():
     pass
 
 
+# 형식화 식에 Dictionary를 사용하면 문장이 번잡스러워진다.
 def fn_11():
+    soup = 'lenti'
+    formatted = 'Today\'s soup is %(soup)s.' % {'soup': soup}
+    print(formatted)
     pass
 
 
+# 긴 형식화 식을 여러 줄로 나눠 써서 하나로 합치고, 형식화에 사용할 Dictionary에 넣을 값을 한 줄에 나열해야 한다.
 def fn_12():
+    menu = {
+        'soup': 'lentil',
+        'oyster': 'tongyoung',
+        'special': 'scnitzel',
+    }
+    template = ("Today\'s soup is %(soup)s, "
+                "buy one get two %(oyster)s oysters, "
+                "and our special entree is %(special)s.")
+    formatted = template % menu
+    print(formatted)
     pass
 
 
+# format 내장 함수 : 오래된 C 스타일 형식화 문자열보다 더 표현력이 좋은 고급 문자열 형식화 기능
 def fn_13():
+    a = 1234.5678
+    formatted = format(a, ',.2f')  # 천 단위의 구분자를 표시
+    print(formatted)
+
+    b = 'my 문자열'
+    formatted = format(b, '^20s')  # ^ : 중앙에 값을 표시
+    print('*', formatted, '*')
     pass
 
 
+# str.format : %d와 같은 C 스타일 형식화 지정자를 사용하는 대신 위치 지정자 {}를 사용할 수 있다.
 def fn_14():
+    key = 'my_var'
+    value = 1.234
+
+    formatted = '{} = {}'.format(key, value)
+    print(formatted)
     pass
 
 
+# 위치 지정자는 콜론 뒤에 형식 지정자를 붙여 넣어 문자열에 값을 넣을 때 어떤 형식으로 변환할 지 정할 수 있다.
 def fn_15():
+    key = 'my_var'
+    value = 1.234
+
+    formatted = '{:<10} = {:.2f}'.format(key, value)
+    print(formatted)
     pass
 
 
+# C 스타일 형식화 문자열에서 %를 표시하고 싶으면 %%로 이스케이프해야 한다.
 def fn_16():
+    print('%.2f%%' % 12.5)
+    print('{} replaces {{}}'.format(1.23))
     pass
 
 
+# 위치 지정자 중괄호에 위치 인덱스, 전달된 인자의 순서를 전달할 수 있다.
 def fn_17():
+    key = 'my_var'
+    value = 1.234
+
+    formatted = '{1} = {0}'.format(key, value)
+    print(formatted)
+
+    # 위치 인덱스를 여러번 사용할 수 있다.
+    name = '철수'
+    formatted = '{0}는 음식을 좋아해. {0}가 요리하는 모습을 봐요.'.format(name)
+    print(formatted)
     pass
 
 
+# 형식화를 하기 전에 값을 변경해야 하는 경우에는 코드 읽기가 어려워진다.
 def fn_18():
+    pantry = [
+        ('아보카도', 1.25),
+        ('바나나', 2.5),
+        ('체리', 15),
+    ]
+
+    for i, (item, count) in enumerate(pantry):
+        old_style = '#%d: %-10s = %d' % (
+            i + 1,
+            item.title(),
+            round(count))
+
+        new_style = '#{}: {:<10s} = {}'.format(
+            i + 1,
+            item.title(),
+            round(count))
+
+    assert old_style == new_style
     pass
 
 
+# str.format과 함께 사용하는 형식 지정자에는 dictionary Key나 list Index를 조합해 사용하거나 유니코드나 repr 문자열로 변환한다.
 def fn_19():
+    menu = {
+        'soup': 'lentil',
+        'oyster': 'tongyoung',
+        'special': 'scnitzel',
+    }
+
+    formatted = '첫번째 글자는 {menu[oyster][0]!r}'.format(
+        menu=menu)
+    print(formatted)
+    pass
+
+
+# f-문자열
+def fn_20():
+    key = 'my_var'
+    value = 1.234
+    formatted = f'{key} = {value}'
+    print(formatted)
+
+    formatted = f'{key!r:<10} = {value:.2f}'
+    print(formatted)
+    pass
+
+
+# f-String에 형식 지정자 옵션을 변수를 사용해 적용 ☆
+def fn_21():
+    places = 3
+    number = 1.23456
+    print(f'내가 고른 숫자는 {number:.{places}f}')
     pass
 
 
@@ -168,7 +266,7 @@ if __name__ == '__main__':
     # fn_7()
     # fn_8()
     # fn_9()
-    fn_10()
+    # fn_10()
     # fn_11()
     # fn_12()
     # fn_13()
@@ -178,4 +276,6 @@ if __name__ == '__main__':
     # fn_17()
     # fn_18()
     # fn_19()
+    # fn_20()
+    # fn_21()
     pass
